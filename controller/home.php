3dc06e9593e5c68cb6611d73b8b393f2d8843b13
Mohammad Momen Mian <56203430+momenmian@users.noreply.github.com>
2021-09-03 21:07:40 +0600

    Version 2.0

---
+++ controller/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+			<title> Login Page </title>
+<?php include 'UI.php';?>
+		</head>
+
+  <body>
+	
+	
+	
+	<section id = "section1">
+		<div class="col-md-10" style="text-align: center; font-size: 100px; color:#2d2244"> JebLu Airways </div><br><br><br>
+                <div class="col-md-10" style="text-align: center; font-size: 26px;"> SIGN IN </div>
+		
+		<form action="signin_process.php" class="form_design" method="post" style="color:White">
+			<div class="col-md-10" style="text-align: center; font-size: 25px; color:#fbf7f7">
+			Username <input type="text" placeholder="Enter Username" name="fname"> <br/>
+			Password <input type="password" placeholder="Enter Password" name="pass"> <br/> <br/>
+			<button type="submit" value="Sign In">Sign In</button> <br><br>
+                        <label> Not a member yet ? </label>
+                        <a href="register.php"> Register </a><br>
+			</div>
+		</form>
+	</section>
+<?php include 'Footer.php';?>
+  </body> 
+</html>
+
